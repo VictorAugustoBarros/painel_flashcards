ea83a7ec4d9494681a9408033a9d72c66b9d757d
VictorAugustoBarros <victor.augustobarros@gmail.com>
2021-09-28 14:31:44 -0300

    first commit

---
+++ src/components/AppFooter.vue
@@ -0,0 +1,15 @@
+<template>
+  <footer>
+    <p>Copyright &copy; "current year" </p>
+  </footer>
+</template>
+
+<style lang="scss">
+#footer p {
+  font-size: 15px;
+  color: #fff;
+  padding-top: 1vw;
+  padding-bottom: 1vw;
+  bottom: 0px;
+}
+</style>
